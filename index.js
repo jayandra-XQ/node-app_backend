fadac6ac65bb354ad6abbffbaa6631f6ab668315
Jayandra Hamal <salonhamal278@gmail.com>
2023-08-10 22:27:59 +0530

    server and read file

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+// server created using nodejs
+const http = require('http'); 
 const fs = require('fs');
 
-
-// to read file
-// fs.readFile('demo.txt','utf-8', (err,txt) => {
-//     console.log(txt)
-// })
+//  to read the static file
+const index = fs.readFileSync('index.html','utf-8'); 
 
 
+// to read the json file
+const data = fs.readFileSync('data.json','utf-8');
 
+const server = http.createServer((req,res) => {
 
-console.log("welcome to node")
-
+    console.log(req.url);
+    
+    // res.setHeader('Dummy','dummyValue');
+    res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Content-Type', 'Text/html');
+    res.end(data);   // we have to convert data into string using json.stringfy
+})
+console.log('server started');
+server.listen(8000);
