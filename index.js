08e0fb4de357277707e7953f8748ac135ea915fc
Jayandra Hamal <salonhamal278@gmail.com>
2023-08-13 22:37:24 +0530

    express js

--- index.js
+++ index.js
@@ -1,22 +1,91 @@
-// server created using nodejs
-const http = require('http'); 
+
 const fs = require('fs');
-
-//  to read the static file
 const index = fs.readFileSync('index.html','utf-8'); 
+const data = JSON.parse(fs.readFileSync('data.json','utf-8'));
+const products = data.products;
+
+// creating server using express
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');  // third party middleware
+
+// bodyParser
+app.use(express.json()); //in-built  middleware
+// app.use(express.urlencoded())
 
 
-// to read the json file
-const data = fs.readFileSync('data.json','utf-8');
+ // used when we send some data through from --- it encoded the url
+//app.use(express.urlencoded());
 
-const server = http.createServer((req,res) => {
+//for logger - 'dev' - 'default'
+app.use(morgan('default'));
+app.use(express.static('public'));
 
-    console.log(req.url);
-    
-    // res.setHeader('Dummy','dummyValue');
-    res.setHeader('Content-Type', 'application/json');
-    // res.setHeader('Content-Type', 'Text/html');
-    res.end(data);   // we have to convert data into string using json.stringfy
+// MiddleWare - Middleware functions are functions that have access to the request object (req), the response object (res), and the next function in the application’s request-response cycle
+// app.use((req, res, next ) => {
+//     console.log( req.method, req.ip, new Date, req.hostname, req.get('User-Agent'))  // Server log
+//     next()
+// })
+
+
+// authorization
+
+const auth = (req,res,next) => {
+    // console.log(req.query)
+    // if (req.body.password == '123') {
+        
+    //     next();
+    // } else {
+    //     res.sendStatus(401);
+    // }
+
+    next();
+}
+
+app.use(auth);
+
+
+// API - ENDPOINT - ROUTE
+
+app.get('/product/:id',auth, (req,res) => {
+    console.log(req.params)
+    res.json({type: 'GET'})
+});
+
+app.post('/',auth, (req,res) => {
+    res.json({type: 'POST'})
+});
+
+app.put('/', (req,res) => {
+    res.json({type: 'PUT'})
+});
+
+app.delete('/',(req,res) => {
+    res.json({type: 'DELETE'})
+});
+
+app.patch('/', (req,res) => {
+    res.json({type: 'PATCH'})
+});
+
+
+
+
+
+
+app.get('/demo', (req,res) => {
+    //res.sendStatus(404);
+    // res.json(products);  // it is widely used bcz we basically use json data
+   //res.status(201).send('<h1>Hello</h1>');  // send  file in the format of html
 })
-console.log('server started');
-server.listen(8000);
+
+
+
+
+
+
+app.listen(8000, () => {
+    console.log('server started');
+});
+
